19904981732fc7015d2e0a763fe0f2e8c362e397
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-19 10:44:18 -0400

    can now add songs

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,35 @@
+class SongsController < ApplicationController
+before_action :authenticate_user!
+
+  def index
+    @user_songs = Song.where(user_id: current_user.id).find_each
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @new_song = Song.new
+  end
+
+  def create
+    @new_song = Song.new(song_params)
+    @new_song.user_id = current_user.id
+    binding.pry
+    if @new_song.save
+      flash[:success] = 'Song saved successfully'
+      redirect_to songs_path
+    else
+      flash[:alert] = 'Problems saving song'
+      @errors = @song.errors.full_messages
+      render 'songs/new'
+    end
+  end
+
+
+  def song_params
+    params.require(:song).permit(:title,
+                                  :body)
+  end
+end
