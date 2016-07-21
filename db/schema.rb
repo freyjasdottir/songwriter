a0473e166241288d6b82935809eda1cdf69b0560
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-21 19:25:19 -0400

    add arts table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720193158) do
+ActiveRecord::Schema.define(version: 20160721224638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "arts", force: :cascade do |t|
+    t.string   "art",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "song_id"
+  end
+
+  add_index "arts", ["song_id"], name: "index_arts_on_song_id", using: :btree
 
   create_table "songs", force: :cascade do |t|
     t.string  "title",   default: "Untitled", null: false
