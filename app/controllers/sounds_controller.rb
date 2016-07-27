d2556760d60e1767603433f8268c6da583ad54c1
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-27 09:32:55 -0400

    wrong hash syntax in API call

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -8,15 +8,18 @@ class SoundsController < ApplicationController
   end
 
   def create
+    binding.pry
     client = Soundcloud.new(client_id: ENV['SOUNDCLOUD_CLIENT_ID'])
     @sound = Sound.new(sound_params)
     @song = Song.find(params[:song_id])
     track_url = @sound.track_url
 
     begin
-      embed_info = client.get('/oembed', url: 'track_url')
+      binding.pry
+      embed_info = client.get('/oembed', url: track_url)
       @sound.embed_info = embed_info['html']
     rescue SoundCloud::ResponseError
+      binding.pry
       flash[:alert] = 'Problems saving sound'
       render 'sounds/new' && return
     end
@@ -24,9 +27,11 @@ class SoundsController < ApplicationController
     @sound.song_id = params['song_id']
 
     if @sound.save
+      binding.pry
       flash[:success] = 'Sound saved successfully'
       redirect_to song_path(params['song_id'])
     else
+      binding.pry
       flash[:alert] = 'Problems saving sound'
       @errors = @sound.errors.full_messages
       render 'sounds/new'
