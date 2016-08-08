05694c4c4ce46c1cc16db28a17afa8a78522138b
Hannah Read <hannahdotread+gituser@gmail.com>
2016-08-08 09:55:02 -0400

    adding tests for art upload no file case

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,4 +1,5 @@
 class ArtsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @art = Art.new
@@ -6,20 +7,30 @@ class ArtsController < ApplicationController
   end
 
   def create
-    @art = Art.new(art_params)
+    @song = Song.find(params[:song_id])
+
+    begin
+      @art = Art.new(art_params)
+    rescue ActionController::ParameterMissing
+      flash[:alert] = 'Must choose a file to upload'
+      @art = Art.new
+      @song = Song.find(params[:song_id])
+      render('arts/new') && return
+    end
+
     @art.song_id = params['song_id']
+
 
     if @art.save
       flash[:success] = 'Art saved successfully'
       redirect_to song_path(params['song_id'])
     else
       flash[:alert] = 'Problems saving Art'
-      @errors = art.errors.full_messages
       render 'arts/new'
     end
   end
 
   def art_params
-    params.require(:art).permit(:art)
+    params.require(:art).permit(:imgfile)
   end
 end
